65bc13e7aa9d1f7f95bd356931edb0909c6006e6
NomLom <leon.sparrow@gmail.com>
2024-02-12 22:06:42 +0000

    some files

--- rebuild.sh
+++ rebuild.sh
@@ -1,1 +1,1 @@
-rebuild switch -I nixpkgs=$HOME/GitHub/nixpkgs -I nixos-xonfig=$HOME/GitHub/nixfiles/hosts/vm1/configuration.nix
+sudo nixos-rebuild switch -I nixpkgs=$HOME/GitHub/SparrowNix/nixpkgs -I nixos-config=$HOME/GitHub/SparrowNix/nixfiles/hosts/vm1/configuration.nix
