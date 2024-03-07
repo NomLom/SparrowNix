7329d89738bf4c769ba24d7c4a7e96bbaca29203
NomLom <leon.sparrow@gmail.com>
2024-03-07 20:44:13 +0000

    plasma 6 nvidia changes

--- modules/plasma.nix
+++ modules/plasma.nix
@@ -8,7 +8,7 @@
 
   # Enable the KDE Plasma Desktop Environment.
   services.xserver.displayManager.sddm.enable = true;
-  #services.xserver.displayManager.sddm.wayland.enable = true;
+  services.xserver.displayManager.sddm.wayland.enable = true;
   services.xserver.desktopManager.plasma6.enable = true;
 
   # Configure keymap in X11
