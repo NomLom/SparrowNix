975050730933b9f2e1fd01eff7a04511d95d11fd
NomLom <leon.sparrow@gmail.com>
2024-03-24 11:35:16 +0000

    Split out Graphics Drivers

--- hosts/slide-desktop/default.nix
+++ hosts/slide-desktop/default.nix
@@ -17,6 +17,7 @@
     ../../modules/smartmon.nix
     ../../modules/plasma6.nix
     ../../modules/starship.nix
+    ../../modules/nvidiaRTX.nix
    # ../../modules/passwordstore.nix
     # ../../modules/satpaper.nix
     ../../modules/nixos/services/default.nix
@@ -78,60 +79,8 @@
   networking.networkmanager.connectionConfig."connection.mdns" = 2;
   services.avahi.enable = true;
 
-
   # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable OpenGL
-  hardware.opengl = {
-    enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
-    extraPackages = with pkgs; [
-      vaapiVdpau
-      libvdpau-va-gl
-      libva
-      vulkan-loader
-    ];
-    extraPackages32 = with pkgs.pkgsi686Linux; [libva];
-    setLdLibraryPath = true;
-  };
-
-  # Load nvidia driver for Xorg and Wayland
-  services.xserver.videoDrivers = ["nvidia"];
-
-  hardware.nvidia.modesetting.enable = true;
-  hardware.nvidia.powerManagement.enable = false;
-  hardware.nvidia.powerManagement.finegrained = false;
-  hardware.nvidia.open = false;
-  hardware.nvidia.nvidiaSettings = true;
-  # Special config to load the latest (535 or 550) driver for the support of the 4070 SUPER
-  hardware.nvidia.package = let
-    rcu_patch = pkgs.fetchpatch {
-      url = "https://github.com/gentoo/gentoo/raw/c64caf53/x11-drivers/nvidia-drivers/files/nvidia-drivers-470.223.02-gpl-pfn_valid.patch";
-      hash = "sha256-eZiQQp2S/asE7MfGvfe6dA/kdCvek9SYa/FFGp24dVg=";
-    };
-  in
-    config.boot.kernelPackages.nvidiaPackages.mkDriver {
-      #  version = "535.154.05";
-      #  sha256_64bit = "sha256-fpUGXKprgt6SYRDxSCemGXLrEsIA6GOinp+0eGbqqJg=";
-      #  sha256_aarch64 = "sha256-G0/GiObf/BZMkzzET8HQjdIcvCSqB1uhsinro2HLK9k=";
-      # openSha256 = "sha256-wvRdHguGLxS0mR06P5Qi++pDJBCF8pJ8hr4T8O6TJIo=";
-      # settingsSha256 = "sha256-9wqoDEWY4I7weWW05F4igj1Gj9wjHsREFMztfEmqm10=";
-      #  persistencedSha256 = "sha256-d0Q3Lk80JqkS1B54Mahu2yY/WocOqFFbZVBh+ToGhaE=";
-
-      version = "550.40.07";
-      sha256_64bit = "sha256-KYk2xye37v7ZW7h+uNJM/u8fNf7KyGTZjiaU03dJpK0=";
-      sha256_aarch64 = "sha256-AV7KgRXYaQGBFl7zuRcfnTGr8rS5n13nGUIe3mJTXb4=";
-      openSha256 = "sha256-mRUTEWVsbjq+psVe+kAT6MjyZuLkG2yRDxCMvDJRL1I=";
-      settingsSha256 = "sha256-c30AQa4g4a1EHmaEu1yc05oqY01y+IusbBuq+P6rMCs=";
-      persistencedSha256 = "sha256-11tLSY8uUIl4X/roNnxf5yS2PQvHvoNjnd2CB67e870=";
-
-      patches = [rcu_patch];
-    };
-
-  #boot.initrd.kernelModules = [ "nvidia" ];
-  #boot.extraModulePackages = [ config.boot.kernelPackages.nvidia_x11 ];
+  #services.printing.enable = true;
 
   programs.thunar.plugins = with pkgs.xfce; [
     thunar-archive-plugin
@@ -153,8 +102,6 @@
   #   enableSSHSupport = true;
   # };
 
-  # List services that you want to enable:
-
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
@@ -164,11 +111,5 @@
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.11"; # Did you read the comment?
 }
